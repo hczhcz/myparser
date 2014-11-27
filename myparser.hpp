545ad19d6a513160a50ddc47c9d6d7b830ecc556
hcz <h.cz@qq.com>
2014-11-27 16:07:29 +0800

    forgot this file

---
+++ myparser.hpp
@@ -0,0 +1,9 @@
+#ifndef MYPARSER_HPP
+#define MYPARSER_HPP
+
+#include "myparser_rule.hpp"
+#include "myparser_pass_filter.hpp"
+#include "myparser_pass_highlight.hpp"
+#include "myparser_pass_repr.hpp"
+
+#endif
