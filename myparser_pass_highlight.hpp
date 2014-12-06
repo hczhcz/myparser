c20f16aa4c0bbdd380311979211463b88747d83f
hcz <h.cz@qq.com>
2014-12-06 23:54:17 +0800

    working

--- myparser_pass_highlight.hpp
+++ myparser_pass_highlight.hpp
@@ -14,8 +14,20 @@ protected:
         out << text;
     }
 
+    virtual void putSpace(const std::string &text) {
+        out << text;
+    }
+
+    virtual void putNumber(const std::string &text) {
+        out << text;
+    }
+
+    virtual void putData(const std::string &text) {
+        out << text;
+    }
+
     virtual void putKeyword(const std::string &text) {
-        out << text << "test"; // TODO
+        out << text;
     }
 
 public:
@@ -26,7 +38,19 @@ public:
 
     // specialized nodes
 
-    // TODO: NodeData<>, if string ... / if number ...
+    template <size_t I>
+    void run(const NodeSpace<I> *node) {
+        putSpace(node->Node::getFullText());
+    }
+
+    template <class T, class E>
+    void run(const NodeData<T, E> *node) {
+        if (std::is_arithmetic<T>::value) {
+            putNumber(node->getText());
+        } else {
+            putData(node->getText());
+        }
+    }
 
     template <class T>
     void run(const NodeTyped<BuiltinKeyword, T> *node) {
@@ -63,9 +87,21 @@ public:
 template <class TX = void> // actually not a template
 class PassHighlight: public Pass<PASS_HIGHLIGHT> { // console ver
 protected:
-    // virtual void putText(const std::string &text) {
-    //     out << style_word << text << style_normal;
-    // }
+    virtual void putSpace(const std::string &text) {
+        out << style_space << text << style_normal;
+    }
+
+    virtual void putNumber(const std::string &text) {
+        out << style_number << text << style_normal;
+    }
+
+    virtual void putData(const std::string &text) {
+        out << style_data << text << style_normal;
+    }
+
+    virtual void putKeyword(const std::string &text) {
+        out << style_keyword << text << style_normal;
+    }
 
 public:
     inline PassHighlight(std::ostream &target):
